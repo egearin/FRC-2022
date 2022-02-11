73f236a8d56cf89816e9cb14577e5c671ea21c6d
egearin <egearingurler646@gmail.com>
2022-02-11 19:52:33 +0300

    DimensionalDrive

---
+++ src/main/java/frc/team6429/subsystems/DimensionalDrive.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.team6429.subsystems;
+
+/** A novel driving method designed by 4th Dimension that utilizes arcade race game controls.
+ * <p> Uses motion profiling, pid, feedforward to build custom high accuracy positioning and velocity control systems.
+ * <p> Has different mode for different drive styles including hand brake, brake, gas, no dead zone controls and more.
+*/
+public class DimensionalDrive {
+
+    /**
+     * Greater control with 
+     */
+    public void hyperDrive(){
+
+    }
+
+    /**
+     * PID assisted drive
+     */
+    public void dimensionalDrive(){
+
+    }
+
+    /**
+     * Combines hyper and dimensional drive controls for maximum control.
+     */
+    public void hyperDimensionalDrive(){
+
+    }
+}
